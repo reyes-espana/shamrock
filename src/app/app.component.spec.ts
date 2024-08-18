2e11efa62350d91a333f1430c0e4df9fea76fe12
unknown <dannyrey2018@gmail.com>
2024-08-18 12:48:49 -0500

    gut app.component.html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,23 +2,23 @@ import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         RouterModule.forRoot([])
-//       ],
-//       declarations: [
-//         AppComponent
-//       ],
-//     }).compileComponents();
-//   });
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterModule.forRoot([])
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
 
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
 //   it(`should have as title 'shamrock'`, () => {
 //     const fixture = TestBed.createComponent(AppComponent);
@@ -32,4 +32,4 @@ import { AppComponent } from './app.component';
 //     const compiled = fixture.nativeElement as HTMLElement;
 //     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, shamrock');
 //   });
-// });
+});
