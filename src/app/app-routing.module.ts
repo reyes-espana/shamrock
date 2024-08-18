99da1e8509388836aa20d25dff557185e419821c
unknown <dannyrey2018@gmail.com>
2024-08-18 13:13:08 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExperienceComponent } from './components/experience/experience.component'
-import { AboutMeComponent } from './components/about-me/about-me.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'about-me', pathMatch: 'full' },
-  { path: 'experience', component: ExperienceComponent },
-  { path: 'about-me', component: AboutMeComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,11 @@ import { MatToolbarModule } from '@angular/material/toolbar'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { ExperienceComponent } from './components/experience/experience.component';
-import { AboutMeComponent } from './components/about-me/about-me.component';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExperienceComponent,
-    AboutMeComponent
   ],
   imports: [
     BrowserModule,
